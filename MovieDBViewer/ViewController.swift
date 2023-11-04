3aceeaacd82733e126308d14d12979bbb87ef49f
tausif51272 <76764842+tausif51272@users.noreply.github.com>
2023-11-04 10:29:41 +0500

    Initial Commit

---
+++ MovieDBViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDBViewer
+//
+//  Created by Aksa on 04/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
